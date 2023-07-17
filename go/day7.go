892ea7a397e28420c17a456f63d29ed5e915826d
Dylan Powers <dylandpowers@gmail.com>
2023-07-17 21:40:47 +0200

    First part of day 7

---
+++ go/day7.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"goaoc22/utils"
+	"strconv"
+	"strings"
+)
+
+type Directory struct {
+	size int
+	children map[string]*Directory
+}
+
+func (dir *Directory) isDir() bool {
+	return dir.size == 0
+}
+
+func MakeDir() *Directory {
+	return &Directory{
+		size: 0,
+		children: map[string]*Directory{},
+	}
+}
+
+func getDirs() *Directory {
+	lines := utils.GetLines("../inputs/day7.txt")
+	stack := []*Directory{}
+	curr := MakeDir()
+
+	for i := 1; i < len(lines); i++ {
+		tokens := strings.Split(lines[i], " ")
+		if tokens[0] == "$" {
+			if tokens[1] == "cd" {
+				if tokens[2] == ".." {
+					curr = stack[len(stack) - 1]
+					stack = stack[:len(stack) - 1]
+				} else {
+					stack = append(stack, curr)
+					if _, ok := curr.children[tokens[2]]; ok {
+						curr = curr.children[tokens[2]]
+					} else {
+						next := MakeDir()
+						curr.children[tokens[2]] = next
+						curr = next
+					}
+				}
+			}
+		} else {
+			if tokens[0] == "dir" {
+				if _, ok := curr.children[tokens[1]]; !ok {
+					curr.children[tokens[1]] = MakeDir()
+				}
+			} else {
+				file := MakeDir()
+				size, err := strconv.Atoi(tokens[0])
+				if err != nil {
+					fmt.Println(err)
+					panic(1)
+				}
+				file.size = size
+				curr.children[tokens[1]] = file
+			}
+		}
+	}
+
+	return stack[0]
+}
+
+func calculateSize(dir *Directory, sizes *[]int) int {
+	size := dir.size
+	for _, d := range dir.children {
+		size += calculateSize(d, sizes)
+	}
+
+	if dir.isDir() && size < 100_000 {
+		*sizes = append(*sizes, size)
+	}
+	return size
+}
+
+func Day7() {
+	sizes := []int{}
+	calculateSize(getDirs(), &sizes)
+	fmt.Println(sizes)
+	total := 0
+	for _, size := range sizes {
+		total += size
+	}
+
+	fmt.Println(total)
+}
